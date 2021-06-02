ee323ce0d1453c8ac42092005d68c6efb122fd4b
admin <vjnaga1411@gmail.com>
2021-06-02 22:10:10 +0530

    //Kotlin Constructor & Inheritance

--- src/main/kotlin/inheritance/methodOverrRiding.kt
+++ src/main/kotlin/inheritance/methodOverrRiding.kt
@@ -1,2 +1,25 @@
 package inheritance
 
+
+open class ParentClass{
+   open fun DerivedMethod(){
+       println("I am from ParentClass")
+   }
+
+}
+
+class Child : ParentClass(){
+
+     override fun DerivedMethod(){
+        println("I am from ChildClass")
+    }
+
+
+}
+fun main(args:Array<String>){
+
+
+    var child =Child()
+    child.DerivedMethod()
+
+}
